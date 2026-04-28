34902be31f384878a3bb0241bc155152e7b3bad1
BeppuKanato <mfm25111@bene.fit.ac.jp>
2026-04-28 15:00:15 +0900

    🎨 lintエラーを修正

--- front/src/lib/firebase.ts
+++ front/src/lib/firebase.ts
@@ -1,6 +1,5 @@
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getAnalytics, isSupported } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyCt1Cf4O-JRaiF7YcRL_0tDqPxdYx7cxUs",
