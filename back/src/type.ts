163d90211589198b019c7d3a30c068098d911935
BeppuKanato <mfm25111@bene.fit.ac.jp>
2026-05-19 17:09:34 +0900

    🐛 ビルドエラーを修正

--- back/src/type.ts
+++ back/src/type.ts
@@ -1,27 +1,27 @@
-import { Difficulty, JudgeType } from "@prisma/client";
+// import { Difficulty, JudgeType } from "@prisma/client";
 
-export type LoginResponse = {
-    id: string
-    name: string
-}
+// export type LoginResponse = {
+//     id: string
+//     name: string
+// }
 
-export type MissionResponse = {
-    id: string,
-    dificulty: Difficulty,
-    title: string,
-    detail: string,
-    component: string,
-}
+// export type MissionResponse = {
+//     id: string,
+//     dificulty: Difficulty,
+//     title: string,
+//     detail: string,
+//     component: string,
+// }
 
-export type AIResponse = {
-    score: number,
-    reason: {
-        "good": string[],
-        "bad": string[],
-    },
-    feedbacks: {
-        index: number,
-        type: JudgeType,
-        text: string,
-    }[],
-}
+// export type AIResponse = {
+//     score: number,
+//     reason: {
+//         "good": string[],
+//         "bad": string[],
+//     },
+//     feedbacks: {
+//         index: number,
+//         type: JudgeType,
+//         text: string,
+//     }[],
+// }
